32f81b82ea5de778b616587f6d5b9530485ecac1
iMentoz <78376392+iMentoz@users.noreply.github.com>
2021-02-04 21:35:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -141,8 +141,8 @@ async function starts() {
 			const content = JSON.stringify(mek.message)
 			const from = mek.key.remoteJid
 			const type = Object.keys(mek.message)[0]
-			const iMentoz = ["6282340533705@s.whatsapp.net"]
-			const Mentoz = mek.message.conversation
+			const FarhanGans = ["628311800241@s.whatsapp.net"]
+			const farhan = mek.message.conversation
 			const insom = from.endsWith('@g.us')
 			const nameReq = insom ? mek.participant : mek.key.remoteJid
 			pushname2 = client.contacts[nameReq] != undefined ? client.contacts[nameReq].vname || client.contacts[nameReq].notify : undefined
